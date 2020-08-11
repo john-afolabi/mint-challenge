8a1fd3f43a13ba7c80007bc586fc652c962a6292
John Afolabi <johnafolabi.da@gmail.com>
2020-08-11 22:40:31 +0100

    Add svg icons to SideNav

--- src/components/Navigation/SideNav.jsx
+++ src/components/Navigation/SideNav.jsx
@@ -1,4 +1,13 @@
 import React from "react";
+import Overview from "../../assets/icons/overview.svg";
+import Payments from "../../assets/icons/payments.svg";
+import ReconciledPayments from "../../assets/icons/reconciled-payments.svg";
+import UnReconciledPayments from "../../assets/icons/unreconciled-payments.svg";
+import ManualSettlement from "../../assets/icons/manual-settlement.svg";
+import AllOrders from "../../assets/icons/all-orders.svg";
+import PendingOrders from "../../assets/icons/pending-orders.svg";
+import ReconciledOrders from "../../assets/icons/reconciled-orders.svg";
+import MerchantProfile from "../../assets/icons/merchant-profile.svg";
 
 const SideNav = () => {
 	return (
@@ -8,24 +17,56 @@ const SideNav = () => {
 			<nav className="side_nav">
 				<ul>
 					<p>Main</p>
-					<li>Overview</li>
+					<li>
+						<img src={Overview} alt="overview" />
+						Overview
+					</li>
 				</ul>
-
 				<ul>
 					<p>Payments</p>
-					<li>All Payments</li>
-					<li>Reconciled Payments</li>
-					<li>Un - Reconciled Payments</li>
-					<li>Manual Settlement</li>
+					<li>
+						<img src={Payments} alt="all payments" />
+						All Payments
+					</li>
+					<li>
+						<img
+							src={ReconciledPayments}
+							alt="reconciled payments"
+						/>
+						Reconciled Payments
+					</li>
+					<li>
+						<img
+							src={UnReconciledPayments}
+							alt="unreconciled payments"
+						/>
+						Un - Reconciled Payments
+					</li>
+					<li>
+						<img src={ManualSettlement} alt="manual settlement" />
+						Manual Settlement
+					</li>
 				</ul>
 				<ul>
 					<p>Orders</p>
-					<li>All Orders</li>
-					<li>Pending Orders</li>
-					<li>Reconciled Orders</li>
+					<li>
+						<img src={AllOrders} alt="all orders" />
+						All Orders
+					</li>
+					<li>
+						<img src={PendingOrders} alt="pending orders" />
+						Pending Orders
+					</li>
+					<li>
+						<img src={ReconciledOrders} alt="reconciled orders" />
+						Reconciled Orders
+					</li>
 				</ul>
 				<ul>
-					<p>Merchant Profile</p>
+					<p>
+						<img src={MerchantProfile} alt="merchant profile" />
+						Merchant Profile
+					</p>
 				</ul>
 			</nav>
 		</div>

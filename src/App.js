e2b9ef73a5705ca00fbcfb1a87f8469eec529073
John Afolabi <johnafolabi.da@gmail.com>
2020-08-12 12:25:40 +0100

    Fix dashboard layout

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import "./assets/styles/app.scss";
 import TopNav from "./components/Navigation/TopNav";
 import SideNav from "./components/Navigation/SideNav";
+import Dashboard from "./components/Dashboard";
 
 function App() {
 	return (
-		<>
+		<div className="App">
 			<TopNav />
-			<SideNav />
-		</>
+			<div style={{ display: "inline-flex" }}>
+				<SideNav />
+				<Dashboard />
+			</div>
+		</div>
 	);
 }
 

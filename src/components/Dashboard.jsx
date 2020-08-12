e9297f73fcc49d2109f46ee269b0f8f90dd1896e
John Afolabi <johnafolabi.da@gmail.com>
2020-08-12 08:51:15 +0100

    Write Dashboard section html layout

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -43,6 +43,44 @@ const Dashboard = () => {
 						<img src={SmallChart} alt="total transaction value" />
 					</div>
 				</div>
+				<div className="main_summary">
+					<div className="main_summary_left">
+						<div className="main_summary_filter">
+							<h3>Today: 9, Aug 2020</h3>
+							<div>
+								<select name="months">
+									<option>1 Jan - 1 Jun</option>
+									<option>1 Jan - 1 Jun</option>
+									<option>1 Jan - 1 Jun</option>
+								</select>
+								<div></div>
+							</div>
+						</div>
+						<div className="data_chart"></div>
+					</div>
+
+					<div className="main_summary_right">
+						<div className="main_summary_card">
+							<h4>Orders</h4>
+
+							<hr />
+							<hr />
+							<p>Pending Orders:</p>
+							<p>Reconciled Orders:</p>
+							<p>Total Orders:</p>
+						</div>
+
+						<div className="main_summary_card">
+							<h4>Payments</h4>
+
+							<hr />
+							<hr />
+							<p>Un-reconciled Payments:</p>
+							<p>Reconciled Payments:</p>
+							<p>Total Payments:</p>
+						</div>
+					</div>
+				</div>
 			</section>
 		</div>
 	);
